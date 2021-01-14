c6d1b4b529c6376d90d668d5bf4afabe64eaac20
Michelle Moon <robbins.michelle@ymail.com>
2021-01-13 18:23:35 -0600

    resources page

--- resource.js
+++ resource.js
@@ -1,21 +1,24 @@
 let addBttnEl = document.getElementById("add");
+let storage = localStorage.getItem("resource");
+let cardEl = document.querySelector(".resource-body");
 
 
 addBttnEl.addEventListener("click",function addResource() {
     event.preventDefault();
     const input = document.querySelector("#link").value;
-    let cardEl = document.querySelector(".resource-body");
-    console.log(input)
     const aEl = document.createElement("a");
     const pEl = document.createElement("p");
     aEl.setAttribute("href", input);
     aEl.textContent= input;
     const deleteBttnEl = document.createElement("button");
-    deleteBttnEl.setAttribute('class', 'delete')
+    deleteBttnEl.setAttribute('class', 'btn');
+    const delIcon = document.createElement("i");
+    delIcon.setAttribute("class", "fa fa-trash");
+    deleteBttnEl.append(delIcon);
     cardEl.append(pEl);
     pEl.append(aEl);
+    pEl.append(deleteBttnEl);
     // store to local storage
-   let allLinks = localStorage.setItem("resource", input);
-   console.log(allLinks)
-
+   localStorage.setItem("resource", val);
+    document.getElementById('form').reset();
 });
